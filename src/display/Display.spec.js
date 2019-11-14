fdf9738f2686508c46a4fddb8dcbea2b019c2537
cladams0203 <cladams0203@gmail.com>
2019-11-13 23:40:09 -0500

    working on the display tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react'
+import Display from './Display';
+import { render } from 'react-testing-library';
+// import '@testing-library/jest-dom/extend-expect';
+
+test("Display Renders without Crashing", () => {
+    render(<Display/>)
+})
+test("Display Snapshot", () => {
+    const wrapper = render(<Display />)
+    expect(wrapper.asFragment()).toMatchSnapshot()
+})
+test("Displays open/closed and locked/unlocked", () => {
+    const wrapper = render(<Display />)
+})
