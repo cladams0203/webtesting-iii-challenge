0f4d2fc632115301446773e76dafa6a46c846311
cladams0203 <cladams0203@gmail.com>
2019-11-13 22:50:16 -0500

    finished tests for dashboard and gate

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,30 @@
 // Test away
+import React from 'react'
+import Dashboard from './Dashboard';
+import {render} from 'react-testing-library'
+
+test("Dashboard Renders Without Crashing", () => {
+    render(<Dashboard />);
+})
+test("Dashboard Snapshot", () => {
+    const wrapper = render(<Dashboard />)
+    expect(wrapper.asFragment()).toMatchSnapshot()
+})
+test("Open and Unlocked by Default", () => {
+    const wrapper = render(<Dashboard/>);
+    expect(wrapper.getByText(/unlocked/i).textContent).toMatch(/unlocked/i)
+    expect(wrapper.getByText(/open/i).textContent).toMatch(/open/i)
+})
+test('Shows Controls and Display', () => {
+    const wrapper = render(<Dashboard />)
+    expect(wrapper.getByText(/unlocked/i).textContent).toMatch(/unlocked/i)
+    expect(wrapper.getByText(/open/i).textContent).toMatch(/open/i)
+    expect(wrapper.getByText(/lock gate/i).textContent).toMatch(/lock gate/i)
+    expect(wrapper.getByText(/close gate/i).textContent).toMatch(/close gate/i)
+})
+test('Cannot be Closed or Opened if Locked', () => {
+    const wrapper = render(<Dashboard/>)
+    const locked = wrapper.queryByText(/unlock gate/i)
+    locked && expect(wrapper.getByText(/locked/i).className).toMatch(/red-led/i)
+    locked && expect(wrapper.getByText(/closed/i).className).toMatch(/red-led/i)
+})
