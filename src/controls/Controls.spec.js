16ce5b2bbdcbe6a9f169d29e988abd2b8028b29c
cladams0203 <cladams0203@gmail.com>
2019-11-14 22:29:33 -0500

    finished controls testing MVP complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,36 @@
 // Test away!
+import React from 'react'
+import Controls from './Controls';
+import { render } from 'react-testing-library';
+import '@testing-library/jest-dom/extend-expect';
+
+test('Controls renders without crashing', () => {
+    render(<Controls/>)
+})
+test('Controls Snapshot', () => {
+    const wrapper = render(<Controls/>)
+    expect(wrapper.asFragment()).toMatchSnapshot()
+})
+test('Buttons display in document', () => {
+    const wrapper = render(<Controls/>)
+    const button1 = wrapper.getByTestId('button1')
+    const button2 = wrapper.getByTestId('button2')
+    expect(button1).toBeInTheDocument()
+    expect(button2).toBeInTheDocument()
+})
+test('Button text reflects state', () => {
+    const wrapper = render(<Controls/>)
+    const button1 = wrapper.getByTestId('button1')
+    const lockbutton = button1.getAttribute('disabled')
+    lockbutton === false ? 
+    expect(button1.textContent).toMatch(/unlock gate/i) :
+    expect(button1.textContent).toMatch(/lock gate/i)
+    const button2 = wrapper.getByTestId('button2')
+    const lockbutton2 = button2.getAttribute('disabled')
+    lockbutton2 === false ? 
+    expect(button2.textContent).toMatch(/open gate/i) :
+    expect(button2.textContent).toMatch(/close gate/i)
+
+})
+
+
