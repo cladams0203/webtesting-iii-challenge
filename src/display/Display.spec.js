a380243e9ec947db924833cdb81d23af1b79bfe1
cladams0203 <cladams0203@gmail.com>
2019-11-14 21:59:01 -0500

    finished display component testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,7 +2,8 @@
 import React from 'react'
 import Display from './Display';
 import { render } from 'react-testing-library';
-// import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
+
 
 test("Display Renders without Crashing", () => {
     render(<Display/>)
@@ -13,4 +14,24 @@ test("Display Snapshot", () => {
 })
 test("Displays open/closed and locked/unlocked", () => {
     const wrapper = render(<Display />)
+   const display1 = wrapper.getByTestId('display1')
+   const display2 = wrapper.getByTestId('display2')
+   expect(display1).toBeInTheDocument()
+   expect(display2).toBeInTheDocument()
+})
+test('Displays Closed if Closed prop is true and red/green class', () => {
+    const wrapper = render(<Display/>)
+    const closed = wrapper.getByTestId('display2')
+    closed.classList.contains('red-led') &&
+    expect(closed.textContent).toMatch(/closed/i)
+    closed.classList.contains('green-led') &&
+    expect(closed.textContent).toMatch(/open/i)
+})
+test('Displays Locked if Locked prop is true and red/green class', () => {
+    const wrapper = render(<Display/>)
+    const locked = wrapper.getByTestId('display1')
+    locked.classList.contains('red-led') &&
+    expect(locked.textContent).toMatch(/locked/i)
+    locked.classList.contains('green-led') &&
+    expect(locked.textContent).toMatch(/unlocked/i)
 })
