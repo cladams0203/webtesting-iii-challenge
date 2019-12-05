0d3bf21ff804791f47cf018615e46985072b4537
cladams0203 <cladams0203@gmail.com>
2019-12-04 21:46:16 -0500

    added button tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,25 +1,25 @@
 // Test away!
 import React from 'react'
 import Controls from './Controls';
-import { render } from 'react-testing-library';
+import * as rtl  from 'react-testing-library';
 import '@testing-library/jest-dom/extend-expect';
 
 test('Controls renders without crashing', () => {
-    render(<Controls/>)
+    rtl.render(<Controls/>)
 })
 test('Controls Snapshot', () => {
-    const wrapper = render(<Controls/>)
+    const wrapper = rtl.render(<Controls/>)
     expect(wrapper.asFragment()).toMatchSnapshot()
 })
 test('Buttons display in document', () => {
-    const wrapper = render(<Controls/>)
+    const wrapper = rtl.render(<Controls/>)
     const button1 = wrapper.getByTestId('button1')
     const button2 = wrapper.getByTestId('button2')
     expect(button1).toBeInTheDocument()
     expect(button2).toBeInTheDocument()
 })
 test('Button text reflects state', () => {
-    const wrapper = render(<Controls/>)
+    const wrapper = rtl.render(<Controls/>)
     const button1 = wrapper.getByTestId('button1')
     const lockbutton = button1.getAttribute('disabled')
     lockbutton === false ? 
@@ -30,6 +30,7 @@ test('Button text reflects state', () => {
     lockbutton2 === false ? 
     expect(button2.textContent).toMatch(/open gate/i) :
     expect(button2.textContent).toMatch(/close gate/i)
+
 })
 
 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,30 +1,48 @@
 // Test away
 import React from 'react'
 import Dashboard from './Dashboard';
-import {render} from 'react-testing-library'
+import Controls from '../controls/Controls'
+import * as rtl from 'react-testing-library'
 
 test("Dashboard Renders Without Crashing", () => {
-    render(<Dashboard />);
+    rtl.render(<Dashboard />);
 })
 test("Dashboard Snapshot", () => {
-    const wrapper = render(<Dashboard />)
+    const wrapper = rtl.render(<Dashboard />)
     expect(wrapper.asFragment()).toMatchSnapshot()
 })
 test("Open and Unlocked by Default", () => {
-    const wrapper = render(<Dashboard/>);
+    const wrapper = rtl.render(<Dashboard/>);
     expect(wrapper.getByText(/unlocked/i).textContent).toMatch(/unlocked/i)
     expect(wrapper.getByText(/open/i).textContent).toMatch(/open/i)
 })
 test('Shows Controls and Display', () => {
-    const wrapper = render(<Dashboard />)
+    const wrapper = rtl.render(<Dashboard />)
     expect(wrapper.getByText(/unlocked/i).textContent).toMatch(/unlocked/i)
     expect(wrapper.getByText(/open/i).textContent).toMatch(/open/i)
     expect(wrapper.getByText(/lock gate/i).textContent).toMatch(/lock gate/i)
     expect(wrapper.getByText(/close gate/i).textContent).toMatch(/close gate/i)
 })
 test('Cannot be Closed or Opened if Locked', () => {
-    const wrapper = render(<Dashboard/>)
+    const wrapper = rtl.render(<Dashboard/>)
     const locked = wrapper.queryByText(/unlock gate/i)
     locked && expect(wrapper.getByText(/locked/i).className).toMatch(/red-led/i)
     locked && expect(wrapper.getByText(/closed/i).className).toMatch(/red-led/i)
+})
+test('checking button functionality', () => {
+    const wrapper = rtl.render(<Controls />)
+    const button2 = wrapper.getByTestId('button2')
+    const button1 = wrapper.getByTestId('button1')
+    rtl.act(() => {
+        rtl.fireEvent.click(button2)
+        
+    })
+     expect(wrapper.getByTestId('button2').textContent).toMatch(/open gate/i)
+
+     rtl.act(() => {
+        rtl.fireEvent.click(button1)
+        
+    })
+     expect(wrapper.getByTestId('button1').textContent).toMatch(/unlock gate/i)
+    
 })
